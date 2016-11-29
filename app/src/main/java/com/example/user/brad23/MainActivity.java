e4755acf22b2aabacad0a9056d84213d509f8ccb
Brad Chao <brad@brad.tw>
2016-11-29 15:34:56 +0800

    v3

--- app/src/main/java/com/example/user/brad23/MainActivity.java
+++ app/src/main/java/com/example/user/brad23/MainActivity.java
@@ -3,19 +3,26 @@ package com.example.user.brad23;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import static android.R.attr.data;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img = (ImageView)findViewById(R.id.img);
 
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.CAMERA)
@@ -38,7 +45,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1 && resultCode == RESULT_OK){
-
+            take1(data);
         }
     }
+
+    private void take1(Intent it){
+        Bitmap bmp = (Bitmap)it.getExtras().get("data");
+        img.setImageBitmap(bmp);
+    }
+
 }
