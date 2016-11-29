0a74b42b3baea37bdb42dd3d5115938267794dfa
Brad Chao <brad@brad.tw>
2016-11-29 16:06:35 +0800

    v5

--- app/src/main/java/com/example/user/brad23/MainActivity.java
+++ app/src/main/java/com/example/user/brad23/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -12,6 +13,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -53,6 +55,11 @@ public class MainActivity extends AppCompatActivity {
         Uri photoUri = Uri.fromFile(photoFile);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
         startActivityForResult(intent, 2);
+    }
+
+    public void test3(View v){
+        Intent it = new Intent(this,CameraActivity.class);
+        startActivityForResult(it, 3);
     }
 
     @Override
