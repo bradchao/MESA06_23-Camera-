23c810748712eca279638ff188145cec6705e80e
Brad Chao <brad@brad.tw>
2016-11-29 15:26:23 +0800

    v2

--- app/src/main/java/com/example/user/brad23/MainActivity.java
+++ app/src/main/java/com/example/user/brad23/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.user.brad23;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,8 +29,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+    public void test1(View v){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK){
 
-
-
+        }
+    }
 }
