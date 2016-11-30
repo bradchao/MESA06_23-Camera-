976d1f6e69794e283f1d4c95641645a4a2388d69
Brad Chao <brad@brad.tw>
2016-11-30 09:51:46 +0800

    v9

--- app/src/main/java/com/example/user/brad23/CameraActivity.java
+++ app/src/main/java/com/example/user/brad23/CameraActivity.java
@@ -3,18 +3,25 @@ package com.example.user.brad23;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class CameraActivity extends AppCompatActivity {
     private FrameLayout frameLayout;
     private MyPreview myPreview;
     private Camera camera;
+    private File sdroot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +46,9 @@ public class CameraActivity extends AppCompatActivity {
     }
 
     private void init(){
+
+        sdroot = Environment.getExternalStorageDirectory();
+
         camera = Camera.open();
         frameLayout = (FrameLayout)findViewById(R.id.frameLayout);
         myPreview = new MyPreview(this,camera);
@@ -53,6 +63,12 @@ public class CameraActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void finish() {
+        setResult(RESULT_OK);
+        super.finish();
+    }
+
     private class MyShutter implements Camera.ShutterCallback {
         @Override
         public void onShutter() {
@@ -63,7 +79,15 @@ public class CameraActivity extends AppCompatActivity {
     private class MyPicCallback implements Camera.PictureCallback {
         @Override
         public void onPictureTaken(byte[] bytes, Camera camera) {
-
+            try {
+                FileOutputStream fout = new FileOutputStream(new File(sdroot, "brad.jpg"));
+                fout.write(bytes);
+                fout.flush();
+                fout.close();
+                Toast.makeText(CameraActivity.this, "Save OK",Toast.LENGTH_SHORT).show();
+            }catch(Exception e){
+                Log.v("brad", e.toString());
+            }
         }
     }
 
--- app/src/main/java/com/example/user/brad23/MainActivity.java
+++ app/src/main/java/com/example/user/brad23/MainActivity.java
@@ -69,6 +69,8 @@ public class MainActivity extends AppCompatActivity {
             take1(data);
         }else if (requestCode == 2 && resultCode == RESULT_OK){
             take2(data);
+        }else if (requestCode == 3 && resultCode == RESULT_OK) {
+            take3(data);
         }
     }
 
@@ -78,6 +80,10 @@ public class MainActivity extends AppCompatActivity {
     }
     private void take2(Intent it){
         Bitmap bmp = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+        img.setImageBitmap(bmp);
+    }
+    private void take3(Intent it){
+        Bitmap bmp = BitmapFactory.decodeFile(new File(sdroot,  "brad.jpg").getAbsolutePath());
         img.setImageBitmap(bmp);
     }
 
